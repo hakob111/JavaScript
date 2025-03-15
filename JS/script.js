9013d74d6c76c277db53e7d7bf826d12c1d6e1ec
hakob.chalabyan-from-norq <chhakob6379@gmail.com>
2025-03-15 14:06:40 +0400

    Second Home work

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,25 @@
 // alert("hello");
 //let result = comfirm("Are you heare?");
 //"use strict";
-let numberofFiles = prompt("How meny films have you woched", "");
+// let numberofFiles = prompt("How meny films have you woched", "");
 
-personalMovieDB = {
-    count: numberofFiles,
-    movies: {},
-    actors: {},
-    generes: [],
-    privat: false
-};
- let lastFilm = prompt("One of the last films which you have woched", "");
- let filmPoint = prompt("How much would you rate it", "");
- let lastFilm2 = prompt("One of the last films which you have woched", "");
- let filmPoint2 = prompt("How much would you rate it", "");
- personalMovieDB["movies"][lastFilm] = filmPoint;
- personalMovieDB["movies"][lastFilm2] = filmPoint2;
+// personalMovieDB = {
+//     count: numberofFiles,
+//     movies: {},
+//     actors: {},
+//     generes: [],
+//     privat: false
+// };
+//  let lastFilm = prompt("One of the last films which you have woched", "");
+//  let filmPoint = prompt("How much would you rate it", "");
+//  let lastFilm2 = prompt("One of the last films which you have woched", "");
+//  let filmPoint2 = prompt("How much would you rate it", "");
+//  personalMovieDB["movies"][lastFilm] = filmPoint;
+//  personalMovieDB["movies"][lastFilm2] = filmPoint2;
 
- console.log(personalMovieDB);
+//  console.log(personalMovieDB);
+
+console.log( NaN || 2 || undefined );
+console.log( NaN && 2 && undefined );
+console.log( 1 && 2 && 3 );
+console.log( !1 && 2 || !3 );
